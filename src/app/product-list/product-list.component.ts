8994ac7ad02f1b26f0d40bb1df231cba729daab8
Kakimbek Nyssanov <kakimbekn@gmail.com>
2024-02-20 12:32:07 +0600

    Lab4 anuglar shop

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,9 +10,25 @@ import { products } from '../products';
 export class ProductListComponent {
   products = [...products];
 
-  share() {
-    window.alert('The product has been shared!');
+
+  share(productLink: string) {
+    const shareMessage = `Check out: ${productLink}`;
+    const whatsappLink = `https://api.whatsapp.com/send?text=${encodeURIComponent(shareMessage)}`;
+    window.location.href = whatsappLink;
+    window.alert('The product has been shared trough whatsapp!');
   }
+
+  sharetg(productLink: string) {
+    const shareMessage = `Check out this product:  ${productLink}`;
+    const tgLink = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
+    window.location.href = tgLink;
+    window.alert('The product has been shared trough telegram!');
+  }
+
+  onNotify(){
+    window.alert('You will be notified when the product goes on sale');
+  }
+
 }
 
 
